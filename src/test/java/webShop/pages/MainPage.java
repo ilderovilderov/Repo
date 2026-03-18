0b34cd07503a386662b123d11e30e0ae05357104
Владислав Юстус <V.Yustus@m-developer.ru>
2026-03-18 13:55:50 +0300

    review

--- src/test/java/webShop/pages/MainPage.java
+++ src/test/java/webShop/pages/MainPage.java
@@ -10,10 +10,14 @@ import static com.codeborne.selenide.Selenide.$$;
 public class MainPage {
 
     private final ElementsCollection computersButton = $$("ul.top-menu li a");
-    private final SelenideElement desktopsButton =  $(byText("Desktops"));
+    private final SelenideElement desktopsButton = $(byText("Desktops"));
 
-    public DesktopsPage clickComputerButton() {
+    public MainPage hoverComputersMenu() {
         computersButton.get(1).hover();
+        return this;
+    }
+
+    public DesktopsPage selectDesktops() {
         desktopsButton.click();
         return new DesktopsPage();
     }
