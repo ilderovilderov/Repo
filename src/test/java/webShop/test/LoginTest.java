97992be79fa4eefa095782ea49af012defca2a26
Alexander I <ilderovilderov@gmail.com>
2026-04-01 03:27:43 +0300

    ..

--- src/test/java/webShop/test/LoginTest.java
+++ src/test/java/webShop/test/LoginTest.java
@@ -24,7 +24,7 @@ public class LoginTest extends TestBase {
 
     @Nested
     public class PositiveTests {
-        @Test
+        @BeforeEach
         @Owner("ilderovilderov")
         @Tag("positive")
         @Severity(CRITICAL)
